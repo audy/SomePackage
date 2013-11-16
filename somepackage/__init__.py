40e4eda92fd2d562a247063e9842b61b8ae4d05b
audy <harekrishna@gmail.com>
2013-11-16 18:26:10 -0500

    you probably want to put your classes in individual files instead of __init__.py

--- somepackage/__init__.py
+++ somepackage/__init__.py
@@ -1,14 +1,1 @@
-class SomeClass:
-    ''' This is a dummy class for testing '''
-
-    @classmethod
-    def some_method(self):
-        '''
-        The purpose of this method is to show how
-        to test methods in SomePackage.
-
-        >>> print some_method()
-        42
-        '''
-
-        return 42
+from someclass import *
---
+++ somepackage/someclass.py
@@ -0,0 +1,14 @@
+class SomeClass:
+    ''' This is a dummy class for testing '''
+
+    @classmethod
+    def some_method(self):
+        '''
+        The purpose of this method is to show how
+        to test methods in SomePackage.
+
+        >>> print some_method()
+        42
+        '''
+
+        return 42
