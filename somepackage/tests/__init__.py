10bb39be21ec5cc05eeab2b63f42f0c5b5794396
audy <harekrishna@gmail.com>
2013-11-16 11:59:20 -0500

    trailing whitespace

--- somepackage/tests/__init__.py
+++ somepackage/tests/__init__.py
@@ -2,14 +2,14 @@
 import unittest
 
 class TestSomething(unittest.TestCase):
+
     def test_something_else(self):
         self.assertEqual(True, True)
-        
+
 def get_suite():
     "Return a unittest.TestSuite."
     import somepackage.tests
-    
+
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromModule(somepackage.tests)
     return suite
-
